cbae42f46b9d6be9ccc5ccd80e3e081755509f2f
Empty Arsenal <matt.merkes@gmail.com>
2014-03-02 15:04:34 -0800

    Cleaned up the JS and moved to index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -34,4 +34,25 @@ $( document ).ready(function() {
         };
     });
 
+    // Add click listener to the left side arrow to open and close 
+    // the advanced search panel
+    $('#pull').click( function(e) {
+       e.preventDefault();
+       // Toggle the advanced search form
+       $('#adv_search').fadeToggle();
+       // Toggle the opening and closing of the search panel
+       $('#search-panel').toggleClass('expandSearch');
+    });
+
+    // Add click listener to the right side arrow to open and close
+    // the advanced search panel
+    $('#push').click( function(e) {
+       e.preventDefault();
+       // Toggle displaying the results
+       $('#results-view').fadeToggle();
+       // Toggle opening and close the results window
+       $('#results-window').toggleClass('expandResults');
+       // Instantiate the accordion
+       $( "#accordion" ).accordion();
+    });
 });
