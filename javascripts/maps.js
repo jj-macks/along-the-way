4b10d896e88acbd2cc2f511352b852a5346fc5d3
Empty Arsenal <matt.merkes@gmail.com>
2014-03-04 10:50:36 -0800

    Changed zoom

--- javascripts/maps.js
+++ javascripts/maps.js
@@ -16,7 +16,7 @@ function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
   var mapOptions = {
-    zoom: 7,
+    zoom: 4,
     center: chicago // this needs to be dynamically defined based on center of route
   }
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
