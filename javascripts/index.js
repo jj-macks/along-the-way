6ec417a41905c4532e6245f4f0ada5af052ade70
kelw <kelw@kelw.local>
2014-03-04 09:53:24 -0800

    updated left search panel

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,28 +1,33 @@
 $( document ).ready(function() {
-    // An array that contains the list of cities entered by the user
-    // cities[0] contains the start destination
-    // cities[1] contains the end destination
-    var cities = [];    
+    // An array that contains the list of places entered by the user
+    // places[0] contains the start destination
+    // places[1] contains the end destination
+    var places = [];    
+    var index = 2;
 
     // Stores start and end destinations entered by user
     // Then removes the overlay containing the form
     $("#go").click(function(e) {
 
-        // if the form is populated with both start and end destinations,
-        // store the values in the cities array
+        // If the form is populated with both start and end destinations,
+        // store the values in the places array
         if ($("#start-destination").val() !== '' &&
             $("#end-destination").val() !== '') {
-            cities[0] = $("#start-destination").val();
-            cities[1] = $("#end-destination").val();
+            places[0] = $("#start-destination").val();
+            places[1] = $("#end-destination").val();
 
-            console.log(cities);    // debugger [Can remove from final version]
+            console.log(places);    // debugger [Can remove from final version]
             
-            $("#initial-destinations-overlay").remove();    // removes the overlay containing the form
-            calcRoute(cities[0], cities[1]);  
+            $("#destinations").html("<label>Destinations</label><input id='add-destination' type='text' placeholder='Add more here'></input><input id='go2' type='button' value='Go!'/><label id='party-list'>Party list:</label><ul></ul>");
+            $("ul").append("<li>" + places[0] + "</li>");
+            $("ul").append("<li>" + places[1] + "</li>");
+
+            //$("#initial-destinations-overlay").remove();    // removes the overlay containing the form
+            calcRoute(places[0], places[1]);                  // draws the initial route between the start and end destinations
             //locationMarker.setMap(null);                    // removes the giant pin marker
-            //draw_initialRoute(cities[0], cities[1]);        // draws the initial route between the start and end destinations 
+            //draw_initialRoute(places[0], places[1]);        // draws the initial route between the start and end destinations 
             
-            console.log(cities);    // debugger [Can remove from final version]
+            console.log(places);    // debugger [Can remove from final version]
             
             e.preventDefault();
 
@@ -33,6 +38,39 @@ $( document ).ready(function() {
             e.preventDefault();
         };
     });
+
+    $(document).on("click", "#go2", function(e) {
+      if (places.length > 10) {
+        alert("Exceeded maximum number of places: 10");
+      } else if ($("#add-destination").val() !== '') {
+        var place = $("#add-destination").val();
+        places[index] = place;
+        index++;
+        $("ul").append("<li>" + place + "</li>");
+        $("#add-destination").val('');
+
+        console.log(places);
+
+        e.preventDefault();
+      } else {
+        alert("Please enter a destination!");
+        e.preventDefault();
+      };
+    });
+
+    $(document).on("click", "li", function(e) {
+      var place = $(this).text();
+
+      console.log(place);           // debugger [Can remove from final version]
+      
+      _.pull(places, place);
+
+      console.log(places);          // debugger [Can remove from final version]
+
+      $(this).remove();
+      e.preventDefault();
+    });
+    
     // Searches set of 10 "route boxes" on click
     $("#next-box-results").click(function(e) {
 
@@ -57,6 +95,21 @@ $( document ).ready(function() {
        // Toggle the opening and closing of the search panel
        $('#search-panel').toggleClass('expandSearch');
     });
+
+    // Add click listener to left side for filter accordion
+    $('#filter-accordion label').click(function(e) {
+      if($('#filter-accordion').is(':visible')) {
+        console.log($('#filter-accordion').is(':visible'));
+        document.getElementById('filter-label').innerHTML = 'Filtering &#8593;';
+        $(e.target).next('div').slideToggle('fast');
+      } else if ($('#filter-accordion').is(':hidden')) {
+        document.getElementById('filter-label').innerHTML = 'Filtering &#8595;';
+        $(e.target).next('div').slideToggle('fast');
+      }
+    });
+
+    // Displays on inital expansion a collapsed filter accordian
+    $('#filter-accordion label').next('div').slideUp();
 
     // Add click listener to the right side arrow to open and close
     // the advanced search panel
