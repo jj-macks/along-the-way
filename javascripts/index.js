79208d7d08ed2e6c849e05b81fa1b5b31bc8dec6
Empty Arsenal <matt.merkes@gmail.com>
2014-03-06 09:40:09 -0800

    Completed controller;

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,2 +1,16 @@
+// Run methods to initialize app
+(function initializeApp() {
+  // Initialize and draw the map
+  app.map.init();
 
+  // Initialize the objects to handle directions
+  app.directions.init();
+
+  // An array that contains the list of places entered by the user.
+  // The places should be in the order the user entered them.
+  app.destinations = [];
+
+  // Initialize the controller, which add listeners to run app
+  app.appController.init();
+})();
 
