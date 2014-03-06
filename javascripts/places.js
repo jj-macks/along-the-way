ff5f7e0952dcad37cc420a4e20e7a9dab1b763fb
Empty Arsenal <matt.merkes@gmail.com>
2014-03-06 10:39:11 -0800

    Cleaned up JS folder

--- javascripts/places.js
+++ javascripts/places.js
@@ -1,35 +1,3 @@
-var markers = [];
-
-var tripResults = [],
-    placesService, // have to wait for map to be defined to define;
-    isFirst = true; 
-
-function placesCallback(results, status){
-  if (isFirst) {
-    //console.log(results.length);
-    for (var i = 0; i < results.length; i++) {
-      tripResults.push([]);
-    }
-    isFirst = false;
-  }
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    console.log("placesCallback");
-
-    for (var i = 0; i < results.length; i++) {
-    //now only returns the first five places for each box
-      var place = results[i];
-      createMarker(place);
-      //now logs the name of each place in the console
-      if(!tripResults[i]) {
-        tripResults[i] = [];
-      }
-      createMarker(place);
-      tripResults[i].push(place);
-    }
-    makeTemplate();
-  }
-}
-
 // creates markers for each result, but can also get result[icon] or whatever
 function createMarker(place) {
   var placeLoc = place.geometry.location;
