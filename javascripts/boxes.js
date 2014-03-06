c41df57029ac0ffb72c9526161ed79e5ce34eec9
Empty Arsenal <matt.merkes@gmail.com>
2014-03-06 09:56:47 -0800

    Removed things

--- javascripts/boxes.js
+++ javascripts/boxes.js
@@ -1,32 +1,4 @@
-var boxes,
-    startAtBox = 0;
-var rboxer = new RouteBoxer();  // draws boxes around the route
-var distance = 5;              // km, we need to set this to be filled by an event listener
 
-// Draw the array of boxes as polylines on the map
-function drawBoxes(boxes) {
-  boxpolys = new Array(boxes.length);
-  for (var i = 0; i < boxes.length; i++) {
-    boxpolys[i] = new google.maps.Rectangle({
-      bounds: boxes[i],
-      fillOpacity: 0,
-      strokeOpacity: 1.0,
-      strokeColor: '#000000',
-      strokeWeight: 1,
-      map: map
-    });
-  }
-}
-
-// Clear boxes currently on the map
-function clearBoxes() {
-  if (boxpolys !== null) {
-    for (var i = 0; i < boxpolys.length; i++) {
-      boxpolys[i].setMap(null);
-    }
-  }
-  boxpolys = null;
-}
 
 function searchTenBoxes(boxes) {
   for (var i = startAtBox; i < startAtBox + 10; i++) {
